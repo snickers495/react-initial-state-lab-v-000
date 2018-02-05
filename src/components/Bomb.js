eb84d6087e0e8d6d5eea0388e70410b1e586fcc7
Lisa Lee <snickers495@gmail.com>
2018-02-05 18:04:37 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,7 +1,7 @@
 // Bomb Component Code Goes Here
 import React from 'react';
 export default class Bomb extends React.Component {
-  constructor(props){
+  constructor(this.props){
     super();
     this.state={
       secondsLeft: props.initialCount;
